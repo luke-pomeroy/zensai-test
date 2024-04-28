9b000c19b1239546b5418f390899326e620227e2
Luke Pomeroy <lukepomeroy@hotmail.co.uk>
2024-04-28 18:14:36 +0100

    feat: add todo model, serializer, view and url

--- server/todos/views.py
+++ server/todos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from todos.models import Todo
 
-# Create your views here.
+from todos.serializers import TodoSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+  queryset = Todo.objects.all()
+  serializer_class = TodoSerializer
